06118161985663f818d4d93ca1f12af02c1795d2
Ethan Jobmann <ethanlouis_fj@yahoo.com>
2017-12-16 12:29:28 -0500

    api setup

--- public/js/app.js
+++ public/js/app.js
@@ -4,8 +4,28 @@ const key = config.key;
 
 app.controller('MainController', ['$http', function($http) {
 
+  this.url = 'https://www.googleapis.com/books/v1/volumes?q=';
+  this.author = 'Anita+Shreve';
 
+  this.getBooks = () => {
+    console.log(key);
 
+    $http({
+      url: this.url + this.author + '&key=' + key,
+      method: 'GET'
+    })
+    .then(response => {
+          console.log(response.data);
+          this.books = response.data;
+        },
+        error => {
+          console.log(error.message);
+        }
+      )
+    .catch(err => console.log(err))
+  }
+
+  this.getBooks();
 
 
 
