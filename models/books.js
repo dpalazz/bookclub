065e534a5367a698ce6593c68b5855d38f709e49
Dana Palazzo <danapalazzo@Danas-MacBook.local>
2017-12-16 14:20:19 -0500

    Updated users and books models

--- models/books.js
+++ models/books.js
@@ -1,7 +1,12 @@
 const mongoose = require('mongoose');
 
 const bookSchema = mongoose.Schema({
-  
+  title:
+  author:
+  publisher:
+  smallThumbnail:
+  retailPrice:
+  user: {type: mongoose.Schema.Types.ObjectId, ref: 'User'}
 });
 
 module.exports = mongoose.model('Book', bookSchema);
--- models/users.js
+++ models/users.js
@@ -2,7 +2,23 @@ const mongoose = require('mongoose');
 const bcrypt = require('bcrypt');
 
 const userSchema = mongoose.Schema({
-
+  username: {type: String, required: true, unique: true},
+  avatar: { type: String, default: "https://i.imgur.com/fGi0Yl0.png"},
+  password: {type: String, required: true}
 });
+
+// Middleware to hash user's password
+userSchema.pre('save', function(next){
+  if (this.isModified('password')) {
+    const hashedPassword = bcrypt.hashSync(this.password, bcrypt.genSaltSync(10));
+    this.password = hashedPassword;
+  }
+  next();
+})
+
+// Compares 'stored' password with entered pasword to authenticate
+userSchema.methods.authenticate = function(password) {
+  return bcrypt.compareSync(password, this.password);
+}
 
 module.exports = mongoose.model('User', userSchema);
