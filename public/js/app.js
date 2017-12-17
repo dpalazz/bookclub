691f8c81342c148f48caa6c9c0adae295dccbbe8
Paige Boyer <paigeboyer@Paiges-MacBook-Pro.local>
2017-12-17 11:54:36 -0500

    created collapser in expanded section

--- public/js/app.js
+++ public/js/app.js
@@ -2,6 +2,32 @@ const app = angular.module('Shelf_Help', ['ngRoute']);
 
 const key = config.key;
 
+// controllers
+app.controller('MainController', ['$http', function($http) {
+
+  this.url = 'https://www.googleapis.com/books/v1/volumes?maxResults=8&printType=books&q=';
+  this.author = 'Steve';
+
+  this.getBooks = () => {
+
+    $http({
+      url: this.url + this.author + '&key=' + key,
+      method: 'GET'
+    })
+    .then(response => {
+          console.log(response.data.items);
+          this.books = response.data.items;
+        },
+        error => {
+          console.log(error.message);
+        }
+      )
+    .catch(err => console.log(err))
+  }
+
+  this.getBooks();
+}]);
+
 app.controller('ExpandedBooksController', ['$http', function($http) {
   this.url = 'https://www.googleapis.com/books/v1/volumes?maxResults=12&startIndex=8&printType=books&q=';
   this.author = 'Steve';
@@ -26,42 +52,16 @@ app.controller('ExpandedBooksController', ['$http', function($http) {
   this.getBooks();
 }]);
 
-// app.controller('BookDisplayController', function() {
-//   // this.books = '000';
-//
-// });
 
+// config
 app.config(['$routeProvider', '$locationProvider', function($routeProvider, $locationProvider) {
   $locationProvider.html5Mode({enabled: true});
 
+
+// routing
   $routeProvider.when('/expandedbooks', {
     templateUrl: 'expanded.html',
     controller: 'ExpandedBooksController',
     controllerAs: 'ctrl'
   });
-}]);
-
-app.controller('MainController', ['$http', function($http) {
-
-  this.url = 'https://www.googleapis.com/books/v1/volumes?maxResults=8&printType=books&q=';
-  this.author = 'Steve';
-
-  this.getBooks = () => {
-
-    $http({
-      url: this.url + this.author + '&key=' + key,
-      method: 'GET'
-    })
-    .then(response => {
-          console.log(response.data.items);
-          this.books = response.data.items;
-        },
-        error => {
-          console.log(error.message);
-        }
-      )
-    .catch(err => console.log(err))
-  }
-
-  this.getBooks();
 }]);
