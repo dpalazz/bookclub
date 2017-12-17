1e711675f7222d0125ae8dbf830d807d4798dcd9
Paige Boyer <paigeboyer@Paiges-MacBook-Pro.local>
2017-12-16 19:49:50 -0500

    completed collapsed index page styling

--- models/books.js
+++ models/books.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 const bookSchema = mongoose.Schema({
   title: '',
-  author: '',
+  authors: '',
   publisher: '',
   smallThumbnail: '',
   retailPrice: '',
