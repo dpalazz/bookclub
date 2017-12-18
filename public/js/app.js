0dbb21ee1c48beb33529a10b065053bc9ce7520a
Dana Palazzo <danapalazzo@Danas-MacBook.local>
2017-12-17 22:05:43 -0500

    Test route for register

--- public/js/app.js
+++ public/js/app.js
@@ -1,12 +1,13 @@
 const app = angular.module('Shelf_Help', ['ngRoute']);
 const key = config.key;
 
-// controllers
+// ===========
+// CONTROLLERS
+// ===========
 app.controller('MainController', ['$http', function($http) {
   this.url = 'https://www.googleapis.com/books/v1/volumes?maxResults=8&printType=books&q=';
   this.author = 'Steve';
   this.book = null;
-
   this.getBooks = () => {
     $http({
       url: this.url + this.author + '&key=' + key,
@@ -22,26 +23,21 @@ app.controller('MainController', ['$http', function($http) {
       )
     .catch(err => console.log(err))
   }
-
   this.getBook = (book) => {
     this.book = null;
     this.book = book;
     console.log(this.book);
   }
-
   this.getBooks();
 }]);
-
 
 app.controller('ExpanderCollapserController', function() {
   this.expanded = false;
 })
 
-
 app.controller('ExpandedBooksController', ['$http', function($http) {
   this.url = 'https://www.googleapis.com/books/v1/volumes?maxResults=12&startIndex=8&printType=books&q=';
   this.author = 'Steve';
-
   this.getBooks = () => {
     $http({
       url: this.url + this.author + '&key=' + key,
@@ -57,17 +53,22 @@ app.controller('ExpandedBooksController', ['$http', function($http) {
   )
   .catch(err => console.log(err))
 }
-
 this.getBooks();
 }]);
 
+app.controller('RegisterController', function() {
+  this.hello = 'hello';
+});
 
-// config
+// ======
+// CONFIG
+// ======
 app.config(['$routeProvider', '$locationProvider', function($routeProvider, $locationProvider) {
   $locationProvider.html5Mode({enabled: true});
 
-
-// routing
+// =======
+// ROUTING
+// =======
   $routeProvider.when('/expandedbooks', {
     templateUrl: 'expanded.html',
     controller: 'ExpandedBooksController',
@@ -80,6 +81,11 @@ app.config(['$routeProvider', '$locationProvider', function($routeProvider, $loc
     controllerAs: 'ctrl'
   });
 
+  $routeProvider.when('/register', {
+    templateUrl: 'register.html',
+    controller: 'RegisterController',
+    controllerAs: 'ctrl'
+  });
 }]);
 
 
