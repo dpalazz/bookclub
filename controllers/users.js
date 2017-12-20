b48f5f398656e2da6a825fa4d95d27744cff864f
Paige Boyer <paigeboyer@Paiges-MacBook-Pro.local>
2017-12-19 20:48:48 -0500

    showing index page

--- controllers/users.js
+++ controllers/users.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router  = express.Router();
 
 const User    = require('../models/users');
-// const Book    = require('../models/books');
+const Book    = require('../models/books');
 
 
 // =======================
--- public/js/app.js
+++ public/js/app.js
@@ -6,58 +6,89 @@ const key = config.key;
 // MAIN CONTROLLER
 // ===============
 app.controller('MainController', ['$http', function($http) {
-  this.url = 'https://www.googleapis.com/books/v1/volumes?maxResults=8&printType=books&q=';
-  this.author = 'Stephen+King';
-  this.book = null;
-  this.search = null;
-  this.showModal = false;
+  // this.url = 'https://www.googleapis.com/books/v1/volumes?maxResults=8&printType=books&q=';
+  // this.author = 'Stephen+King';
+  // this.book = null;
+  // this.search = null;
+  // this.showModal = false;
+  // this.getBooks = () => {
+  //   $http({
+  //     url: this.url + this.author + '&key=' + key,
+  //     method: 'GET'
+  //   })
+  //   .then(response => {
+  //         console.log(response.data.items);
+  //         this.books = response.data.items;
+  //       },
+  //       error => {
+  //         console.log(error.message);
+  //       }
+  //     )
+  //   .catch(err => console.log(err))
+  // }
+  //
+  // this.getBook = (book) => {
+  //   this.book = null;
+  //   this.book = book;
+  //   console.log(this.book);
+  // }
+  //
+  // this.getBooks();
+  //
+  // this.deleteBook = (id) => {
+  //   $http({
+  //     url: '/delete/' + id,
+  //     method: 'DELETE',
+  //     data: id
+  //   }).then((data) => {
+  //     this.getBooks();
+  //   }, ( error ) => {
+  //     console.log(error);
+  //   }).catch(err => console.log(err));
+  // }
+  //
+  // this.searchAPI = () => {
+  //   $http({
+  //     url: this.url + this.search + '&key=' + key,
+  //     method: 'GET'
+  //   }).then((response) => {
+  //     console.log('Results:', response.data.items);
+  //     this.search = null;
+  //   }, ( error ) => {
+  //     console.log(error);
+  //   }).catch(err => console.log(err));
+  // }
+
+  // index
   this.getBooks = () => {
     $http({
-      url: this.url + this.author + '&key=' + key,
+      url: 'books/',
       method: 'GET'
-    })
-    .then(response => {
-          console.log(response.data.items);
-          this.books = response.data.items;
-        },
-        error => {
-          console.log(error.message);
-        }
-      )
-    .catch(err => console.log(err))
-  }
-
-  this.getBook = (book) => {
-    this.book = null;
-    this.book = book;
-    console.log(this.book);
+    }).then(response => {
+      this.books = response.data
+      console.log(this.books);
+    }, error => {
+      console.log(error.message);
+    }).catch(err => console.log(err))
   }
 
   this.getBooks();
 
-  this.deleteBook = (id) => {
+  // my shelf
+  this.getBooks = (id) => {
     $http({
-      url: '/delete/' + id,
-      method: 'DELETE',
-      data: id
-    }).then((data) => {
-      this.getBooks();
-    }, ( error ) => {
-      console.log(error);
-    }).catch(err => console.log(err));
-  }
-
-  this.searchAPI = () => {
-    $http({
-      url: this.url + this.search + '&key=' + key,
+      url: 'books/user/' + req.params.id,
       method: 'GET'
-    }).then((response) => {
-      console.log('Results:', response.data.items);
-      this.search = null;
-    }, ( error ) => {
-      console.log(error);
-    }).catch(err => console.log(err));
+    }).then(response => {
+      this.books = response.data
+      console.log(this.books);
+    }, error => {
+      console.log(error.message);
+    }).catch(err => console.log(err))
   }
+  
+
+
 }]);
 
 
@@ -65,33 +96,33 @@ app.controller('MainController', ['$http', function($http) {
 // OTHER CONTROLLERS
 // =================
 // expand/collapse arrows
-app.controller('ExpanderCollapserController', function() {
-  this.expanded = false;
-});
-
-// expanded index
-app.controller('ExpandedBooksController', ['$http', function($http) {
-  this.url = 'https://www.googleapis.com/books/v1/volumes?maxResults=12&startIndex=8&printType=books&q=';
-  this.author = 'Stephen+King';
-  this.getBooks = () => {
-    $http({
-      url: this.url + this.author + '&key=' + key,
-      method: 'GET'
-    }).then(response => {
-      console.log(response.data.items);
-      this.books = response.data.items;
-    }, error => {
-      console.log(error.message);
-    }).catch(err => console.log(err))
-  }
-
-  this.getBook = (book) => {
-    this.book = book;
-    console.log(this.book)
-  };
-
-  this.getBooks();
-}]);
+// app.controller('ExpanderCollapserController', function() {
+//   this.expanded = false;
+// });
+//
+// // expanded index
+// app.controller('ExpandedBooksController', ['$http', function($http) {
+//   this.url = 'https://www.googleapis.com/books/v1/volumes?maxResults=12&startIndex=8&printType=books&q=';
+//   this.author = 'Stephen+King';
+//   this.getBooks = () => {
+//     $http({
+//       url: this.url + this.author + '&key=' + key,
+//       method: 'GET'
+//     }).then(response => {
+//       console.log(response.data.items);
+//       this.books = response.data.items;
+//     }, error => {
+//       console.log(error.message);
+//     }).catch(err => console.log(err))
+//   }
+//
+//   this.getBook = (book) => {
+//     this.book = book;
+//     console.log(this.book)
+//   };
+//
+//   this.getBooks();
+// }]);
 
 // register
 app.controller('RegisterController', ['$http', function($http) {
@@ -109,57 +140,57 @@ app.controller('RegisterController', ['$http', function($http) {
 }]);
 
 // user's shelf
-app.controller('UserShelfController', ['$http', function($http) {
-  this.url = "https://www.googleapis.com/books/v1/volumes/";
-  this.userBooks = [];
-  this.book = {};
+// app.controller('UserShelfController', ['$http', function($http) {
+//   this.url = "https://www.googleapis.com/books/v1/volumes/";
+//   this.userBooks = [];
+//   this.book = {};
+//
+//   this.getBook = (bookId) => {
+//     $http({
+//       url: this.url + bookId+ '?key=' + key,
+//       method: 'GET'
+//     }).then(response => {
+//       this.book.title = response.data.volumeInfo.title;
+//       this.book.authors = response.data.volumeInfo.authors;
+//       this.book.img = response.data.volumeInfo.imageLinks.thumbnail;
+//       this.book.description = response.data.volumeInfo.description;
+//       this.book.categories = response.data.volumeInfo.categories;
+//       this.book.pageCount = response.data.volumeInfo.pageCount;
+//       this.book.publishedDate = response.data.volumeInfo.publishedDate;
+//       console.log(this.book);
+//       this.userBooks.push(this.book);
+//       this.book = {};
+//     }, error => {
+//       console.error(error)
+//     }).catch(err => console.log(err))
+//   }
+//
+//   this.getUser = (id) => {
+//     $http({
+//       url: '/users/' + id,
+//       method: 'GET'
+//     }).then(response => {
+//       this.user = response.data.user;
+//       console.log(this.user);
+//
+//
+//       for (let i = 0; i < this.user.bookCollection.length; i++) {
+//         console.log(this.user.bookCollection[i]);
+//         this.getBook(this.user.bookCollection[i]);
+//         console.log(this.book);
+//         console.log(this.userBooks);
+//       }
+//       console.log(this.userBooks);
+//     }, error => {
+//       console.error(error);
+//     }).catch(err => console.log(err))
+//
+//
+//   }
+//
+//   this.getUser('5a38037b6c03034b8c7e5ac3');
 
-  this.getBook = (bookId) => {
-    $http({
-      url: this.url + bookId+ '?key=' + key,
-      method: 'GET'
-    }).then(response => {
-      this.book.title = response.data.volumeInfo.title;
-      this.book.authors = response.data.volumeInfo.authors;
-      this.book.img = response.data.volumeInfo.imageLinks.thumbnail;
-      this.book.description = response.data.volumeInfo.description;
-      this.book.categories = response.data.volumeInfo.categories;
-      this.book.pageCount = response.data.volumeInfo.pageCount;
-      this.book.publishedDate = response.data.volumeInfo.publishedDate;
-      console.log(this.book);
-      this.userBooks.push(this.book);
-      this.book = {};
-    }, error => {
-      console.error(error)
-    }).catch(err => console.log(err))
-  }
-
-  this.getUser = (id) => {
-    $http({
-      url: '/users/' + id,
-      method: 'GET'
-    }).then(response => {
-      this.user = response.data.user;
-      console.log(this.user);
-
-
-      for (let i = 0; i < this.user.bookCollection.length; i++) {
-        console.log(this.user.bookCollection[i]);
-        this.getBook(this.user.bookCollection[i]);
-        console.log(this.book);
-        console.log(this.userBooks);
-      }
-      console.log(this.userBooks);
-    }, error => {
-      console.error(error);
-    }).catch(err => console.log(err))
-
-
-  }
-
-  this.getUser('5a38037b6c03034b8c7e5ac3');
-
-}]);
+// }]);
 
 
 // =================
@@ -171,11 +202,11 @@ app.config(['$routeProvider', '$locationProvider', function($routeProvider, $loc
 // =======
 // ROUTING
 // =======
-  $routeProvider.when('/expandedbooks', {
-    templateUrl: 'partials/expanded.html',
-    controller: 'ExpandedBooksController',
-    controllerAs: 'ctrl'
-  });
+  // $routeProvider.when('/expandedbooks', {
+  //   templateUrl: 'partials/expanded.html',
+  //   controller: 'ExpandedBooksController',
+  //   controllerAs: 'ctrl'
+  // });
 
   $routeProvider.when('/register', {
     templateUrl: 'partials/register.html',
