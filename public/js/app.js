b2b7f979056d79209e5a79561aae5600c0074755
Paige Boyer <paigeboyer@Paiges-MacBook-Pro.local>
2017-12-21 09:56:05 -0500

    fixed search results grid

--- public/js/app.js
+++ public/js/app.js
@@ -5,61 +5,7 @@ const app = angular.module('Shelf_Help', ['ngRoute', 'angular.filter']);
 // ===============
 app.controller('MainController', ['$http', function($http) {
 
-  // this.url = 'https://www.googleapis.com/books/v1/volumes?maxResults=8&printType=books&q=';
-  // this.author = 'Stephen+King';
-  // this.book = null;
-  // this.search = null;
-  // this.showModal = false;
-  // this.getBooks = () => {
-  //   $http({
-  //     url: this.url + this.author + '&key=' + key,
-  //     method: 'GET'
-  //   })
-  //   .then(response => {
-  //         console.log(response.data.items);
-  //         this.books = response.data.items;
-  //       },
-  //       error => {
-  //         console.log(error.message);
-  //       }
-  //     )
-  //   .catch(err => console.log(err))
-  // }
-  //
-  // this.getBook = (book) => {
-  //   this.book = null;
-  //   this.book = book;
-  //   console.log(this.book);
-  // }
-  //
-  // this.getBooks();
-  //
-  // this.deleteBook = (id) => {
-  //   $http({
-  //     url: '/delete/' + id,
-  //     method: 'DELETE',
-  //     data: id
-  //   }).then((data) => {
-  //     this.getBooks();
-  //   }, ( error ) => {
-  //     console.log(error);
-  //   }).catch(err => console.log(err));
-  // }
-  //
-  // this.searchAPI = () => {
-  //   $http({
-  //     url: this.url + this.search + '&key=' + key,
-  //     method: 'GET'
-  //   }).then((response) => {
-  //     console.log('Results:', response.data.items);
-  //     this.search = null;
-  //   }, ( error ) => {
-  //     console.log(error);
-  //   }).catch(err => console.log(err));
-  // }
-
   // index
-
   this.getBooks = () => {
     $http({
       url: 'books/',
@@ -115,35 +61,6 @@ app.controller('MainController', ['$http', function($http) {
 // =================
 // OTHER CONTROLLERS
 // =================
-// expand/collapse arrows
-// app.controller('ExpanderCollapserController', function() {
-//   this.expanded = false;
-// });
-//
-// // expanded index
-// app.controller('ExpandedBooksController', ['$http', function($http) {
-//   this.url = 'https://www.googleapis.com/books/v1/volumes?maxResults=12&startIndex=8&printType=books&q=';
-//   this.author = 'Stephen+King';
-//   this.getBooks = () => {
-//     $http({
-//       url: this.url + this.author + '&key=' + key,
-//       method: 'GET'
-//     }).then(response => {
-//       console.log(response.data.items);
-//       this.books = response.data.items;
-//     }, error => {
-//       console.log(error.message);
-//     }).catch(err => console.log(err))
-//   }
-//
-//   this.getBook = (book) => {
-//     this.book = book;
-//     console.log(this.book)
-//   };
-//
-//   this.getBooks();
-// }]);
-
 // register, login, logout
 app.controller('RegisterController', ['$route', '$http', function($route, $http) {
 
@@ -207,7 +124,7 @@ app.controller('SearchController', ['$http', function($http) {
     this.apikey = response.data.key;
   })
 
-  this.url = 'https://www.googleapis.com/books/v1/volumes?maxResults=8&printType=books&q=';
+  this.url = 'https://www.googleapis.com/books/v1/volumes?maxResults=8&orderBy=relevance&printType=books&q=';
 
   this.searchAPI = () => {
     $http({
@@ -278,29 +195,5 @@ app.controller('UserShelfController', ['$http', function($http) {
       console.log(error.message);
     }).catch(err => console.log(err))
   }
-
-}]);
-
-
-// =================
-// CONFIG CONTROLLER
-// =================
-app.config(['$routeProvider', '$locationProvider', function($routeProvider, $locationProvider) {
-  $locationProvider.html5Mode({enabled: true});
-
-// =======
-// ROUTING
-// =======
-  // $routeProvider.when('/expandedbooks', {
-  //   templateUrl: 'partials/expanded.html',
-  //   controller: 'ExpandedBooksController',
-  //   controllerAs: 'ctrl'
-  // });
-
-  // $routeProvider.when('/register', {
-  //   templateUrl: 'partials/register.html',
-  //   controller: 'RegisterController',
-  //   controllerAs: 'ctrl'
-  // });
 
 }]);
